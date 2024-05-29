25f819e5816d12047d1865759f14e37af55e986c
julianaIUD <juliana.pena@iudigital.edu.co>
2024-05-29 16:28:29 -0500

    organizando vista principal

--- src/views/dashboard/Table.tsx
+++ src/views/dashboard/Table.tsx
@@ -119,39 +119,22 @@ const DashboardTable = () => {
         <Table sx={{ minWidth: 800 }} aria-label='table in dashboard'>
           <TableHead>
             <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Date</TableCell>
-              <TableCell>Salary</TableCell>
-              <TableCell>Age</TableCell>
-              <TableCell>Status</TableCell>
+              <TableCell>Nombre</TableCell>
+              <TableCell>break</TableCell>
+              <TableCell>Almuerzo</TableCell>
+              <TableCell>Pausa Activa</TableCell>
+              <TableCell>Formacion</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row: RowType) => (
-              <TableRow hover key={row.name} sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
-                <TableCell sx={{ py: theme => `${theme.spacing(0.5)} !important` }}>
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography sx={{ fontWeight: 500, fontSize: '0.875rem !important' }}>{row.name}</Typography>
-                    <Typography variant='caption'>{row.designation}</Typography>
-                  </Box>
+              <TableRow hover sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
+                <TableCell>JOHN 
                 </TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.date}</TableCell>
-                <TableCell>{row.salary}</TableCell>
-                <TableCell>{row.age}</TableCell>
-                <TableCell>
-                  <Chip
-                    label={row.status}
-                    color={statusObj[row.status].color}
-                    sx={{
-                      height: 24,
-                      fontSize: '0.75rem',
-                      textTransform: 'capitalize',
-                      '& .MuiChip-label': { fontWeight: 500 }
-                    }}
-                  />
-                </TableCell>
+                <TableCell>8:00 am</TableCell>
+                <TableCell>12:00 pm</TableCell>
+                <TableCell>3:40 pm</TableCell>
+                <TableCell>N.A</TableCell>
               </TableRow>
             ))}
           </TableBody>
--- src/views/dashboard/Trophy.tsx
+++ src/views/dashboard/Trophy.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import { styled, useTheme } from '@mui/material/styles'
+import {  Box } from '@mui/material';
 
 // Styled component for the triangle shaped background image
 const TriangleImg = styled('img')({
@@ -25,23 +26,24 @@ const Trophy = () => {
   // ** Hook
   const theme = useTheme()
 
-  const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
+  //const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
 
   return (
     <Card sx={{ position: 'relative' }}>
       <CardContent>
-        <Typography variant='h6'>Congratulations John! 🥳</Typography>
+        <Typography variant='h6'>Bienvenido a Chronos 🥳</Typography>
         <Typography variant='body2' sx={{ letterSpacing: '0.25px' }}>
-          Best seller of the month
+        "Valora cada minuto de tu jornada laboral, pues el tiempo bien empleado no solo impulsa nuestro progreso, sino que también nos acerca a nuestros objetivos con mayor eficiencia y satisfacción."
         </Typography>
-        <Typography variant='h5' sx={{ my: 4, color: 'primary.main' }}>
-          $42.8k
-        </Typography>
+        <Box sx={{ display: 'flex', gap: 1, mt: 2 }}>
         <Button size='small' variant='contained'>
-          View Sales
+          Marcar Entrada
         </Button>
-        <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
-        <TrophyImg alt='trophy' src='/images/misc/trophy.png' />
+        <br></br>
+        <Button size='small' variant='contained'>
+          Marcar Salida
+        </Button>
+        </Box>
       </CardContent>
     </Card>
   )
